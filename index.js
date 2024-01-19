53cbf6f6fafc9671f9139ca15d56681fe74bd241
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-01-19 11:50:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+// const port = 3000;
+
+app.get('/',(req,res) => {
+   res.send('Hello Harsh');
+})
+app.listen(process.env.PORT,() => {
+    console.log(`express is working on port ${process.env.PORT}`);
+})
