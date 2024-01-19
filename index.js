3124c2d8d9340923c6e568e27052272247524a74
Siddhi yadav <113456549+Siddhi1kcr@users.noreply.github.com>
2024-01-19 11:53:11 +0530

    Change name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 // const port = 3000;
 
 app.get('/',(req,res) => {
-   res.send('Hello Harsh');
+   res.send('Hello Siddhi');
 })
 app.listen(process.env.PORT,() => {
     console.log(`express is working on port ${process.env.PORT}`);
